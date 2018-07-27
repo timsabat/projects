2d7293fee63268618b92a0979bf1faf4ec5f4d95
unknown <timsabat>
2018-07-27 18:49:51 +0200

    klienci.sql

---
+++ klienci.sql
@@ -0,0 +1,73 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Czas generowania: 27 Lip 2018, 18:43
+-- Wersja serwera: 10.1.34-MariaDB
+-- Wersja PHP: 7.2.7
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Baza danych: `crud`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `klienci`
+--
+
+CREATE TABLE `klienci` (
+  `id` int(11) NOT NULL,
+  `imie` text COLLATE utf8_polish_ci NOT NULL,
+  `nazwisko` text COLLATE utf8_polish_ci NOT NULL,
+  `telefon` int(11) NOT NULL,
+  `email` text COLLATE utf8_polish_ci NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+
+--
+-- Zrzut danych tabeli `klienci`
+--
+
+INSERT INTO `klienci` (`id`, `imie`, `nazwisko`, `telefon`, `email`) VALUES
+(5, 'Marian', 'Pawelski', 445757840, 'marianek@op.com'),
+(4, 'Mariusz', 'Pudzianowski', 666777888, 'mariusz@pu.com'),
+(3, 'Janek', 'Dzbankiewicz', 300400343, 'janek@dzbnk.pl'),
+(2, 'Marek', 'Kowalski', 500600500, 'm@k.com'),
+(1, 'Janusz', 'Januszewski', 400500400, 'j@j.com');
+
+--
+-- Indeksy dla zrzutów tabel
+--
+
+--
+-- Indeksy dla tabeli `klienci`
+--
+ALTER TABLE `klienci`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT dla tabeli `klienci`
+--
+ALTER TABLE `klienci`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
